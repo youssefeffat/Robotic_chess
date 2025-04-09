33198cf18ba7ba5bf81c122e3263b37c0988778a
Anas <anas.daggag@gmail.com>
2025-04-10 01:15:58 +0200

    version qui demultiplie un mouvement en plusieurs elementaireé

---
+++ hardware/donnees.py
@@ -0,0 +1,152 @@
+
+import struct
+
+SERIAL_BAUDRATE = 921600
+
+# Listes de tous les id :
+ID_ACK_GENERAL                 = 0xC0 # Ack pour tous le reste
+ID_REPEAT_REQUEST              = 0xD0
+
+ID_CMD_MOVE                    = 0xA0 # On envoit le mouvement x y z à faire en millimetres
+ID_ACK_CMD_MOVE                = 0xA1 # On reçoit l'accusé de réception
+
+ID_SERVO_GRAB                  = 0xA2 # On demande de serrer la pince
+ID_ACK_SERVO_GRAB              = 0xA3 # On reçoit l'accusé de réception
+
+ID_SERVO_RELEASE               = 0xA4 # On demande de desserrer la pince
+ID_ACK_SERVO_RELEASE           = 0xA5 # On reçoit l'accusé de réception
+
+ID_HOMING                      = 0xA6 # On demande de faire le homing
+ID_ACK_HOMING                  = 0xA7 # On réçoit l'accusé de réception
+
+ID_SEND_CURRENT_POSITION       = 0xA8 # On demande d'envoyer la position courante
+ID_ACK_SEND_CURRENT_POSITION   = 0xA9 # On reçoit la position courante
+
+ID_CMD_BOUTTON_STATE = 0xB1  # Request button state
+
+idComEnText = {
+    0x00: "",
+    0xA0: "ID_CMD_MOVE",
+    0xA1: "ID_ACK_CMD_MOVE",
+    0xA2: "ID_SERVO_GRAB",
+    0xA3: "ID_ACK_SERVO_GRAB",
+    0xA4: "ID_SERVO_RELEASE",
+    0xA5: "ID_ACK_SERVO_RELEASE",
+    0xA6: "ID_HOMING",
+    0xA7: "ID_ACK_HOMING",
+    0xA8: "ID_SEND_CURRENT_POSITION",
+    0xA9: "ID_ACK_SEND_CURRENT_POSITION",
+    0xC0: "ID_ACK_GENERAL",
+    0xD0: "ID_REPEAT_REQUEST",
+}
+
+class Message():
+    def __init__(self, id=0, length=0, data=None):
+        self.id = id
+        self.len = length
+        self.data = data if data else []
+        self.checksum = 0 #Le checksum est un XOR de tous les octect du message (de id, len et data[])
+    
+    def setData(self, id, length = 0, data = None):
+        self.id = id
+        self.len = length
+        self.data = data if data else []
+
+    def build_packet(self):
+        # Calculate checksum
+        self.checksum = (self.id ^ self.len) & 0xFF
+        for i in range(self.len):
+            self.checksum ^= self.data[i]
+        length = self.len if(self.len) else 1 #Comme ça meme si on envoit un commande sans data, il faut forcément qu'il y est data0
+        # Construct the packet with HEADEAR, ID, length, data, checksum, and FOOTER
+        #For example : (FF A0 02 01 02 A1 FF)
+        packet_format = f'<B B B {length}s B B'
+        packet_data = bytes(self.data)
+        return struct.pack(packet_format, 0xFF, self.id, self.len, packet_data, self.checksum, 0xFF)
+
+    def __str__(self):
+        return f"[FF][{self.id:02X}][{self.len:02X}][{''.join(f'{byte:02X}' for byte in self.data)}][{self.checksum:02X}][FF]"
+
+SIZE_FIFO = 32 #Une FIFO est un buffer, la taille du buffer de reception est de 32
+
+class Position():
+    def __init__(self, x = 0, y = 0, z = 0):
+        self.x = float(x) #Position en metres
+        self.y = float(y) #Position en metres
+        self.z = float(z) #Position en metres
+    
+    def __str__(self):
+        return f"Position(x: {self.x}, y: {self.y}, z: {self.z})"
+
+
+
+
+
+TAILLE_CARREAU = 0.05 #En metres
+HAUTEUR_BRAS = -0.1 #En metres, hauteur du bras à laquelle descendre pour prendre une pièce, en z, à mesurer!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+class ChessboardMoves():
+    def __init__(self):
+        self.moves = ["" for _ in range(SIZE_FIFO)]
+        self.cursor_move_write = 0
+        self.cursor_move_read = 0
+        
+        self.posA1 = Position(0, 0, 0) #Position de A1, utilisé comme référence, à mesurer!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+    def addMoves(self, move):
+        self.moves[self.cursor_move_write] = move
+        self.cursor_move_write = (self.cursor_move_write + 1) % SIZE_FIFO
+    
+    def thereIsNewMoveToDo(self):
+        if (self.cursor_move_write - self.cursor_move_read):
+            return True
+        else:
+            return False
+    
+    def getNextMove(self):
+        if(self.thereIsNewMoveToDo() == False):
+            return ""
+        move = self.moves[self.cursor_move_read]
+        self.moves[self.cursor_move_read] = ""
+        self.cursor_move_read = (self.cursor_move_read + 1) % SIZE_FIFO
+        return move
+
+    def convertMovesToPositions(self, move : str):#On reçoit par exemple "e2e4" et on renvoit la position de e2 et e4
+        #On reçoit par exemple "e2e4" et on
+        #On renvoit la position de e2 et e4        
+        def chessNotationToPosition(notation):
+            if len(notation) != 2:
+                raise ValueError("Invalid chess notation. Expected format: 'e2'")
+            column = notation[0].lower()
+            row = notation[1]
+            if column < 'a' or column > 'h' or row < '1' or row > '8':
+                raise ValueError("Invalid chess notation. Out of bounds.")
+            x = self.posA1.x + (ord(column) - ord('a')) * TAILLE_CARREAU
+            y = self.posA1.y + (int(row) - 1) * TAILLE_CARREAU
+            return Position(x, y, 0)
+        
+        start_pos = chessNotationToPosition(move[:2])
+        end_pos = chessNotationToPosition(move[2:])
+        return start_pos, end_pos
+
+    def convertPositionToChessNotation(self, position: Position):
+        # Convert the position to chess notation
+        column = chr(int((position.x - self.posA1.x) / TAILLE_CARREAU) + ord('a'))
+        row = str(int((position.y - self.posA1.y) / TAILLE_CARREAU) + 1)
+        return f"{column}{row}"
+
+if __name__ == "__main__":
+    # Test the ChessboardMoves class
+    chessboard_moves = ChessboardMoves()
+    chessboard_moves.addMoves("a1a2")
+    chessboard_moves.addMoves("a2e2")
+    chessboard_moves.addMoves("e2e4")
+    chessboard_moves.addMoves("e4f3")
+    chessboard_moves.addMoves("f3b8")
+    chessboard_moves.addMoves("b8a1")
+    
+    while chessboard_moves.thereIsNewMoveToDo():
+        move = chessboard_moves.getNextMove()
+        start_pos, end_pos = chessboard_moves.convertMovesToPositions(move)
+        print(f"Move: {move}, Start Position: {start_pos}, End Position: {end_pos}")
+        print(f"Chess Notation: {chessboard_moves.convertPositionToChessNotation(start_pos)}, {chessboard_moves.convertPositionToChessNotation(end_pos)}")
--- hardware/robotic_arm.py
+++ hardware/robotic_arm.py
@@ -1,20 +1,67 @@
-from core.interfaces import IRoboticArmModule
+
+from core.interfaces import IRoboticArmModule #Si ça ne marche pas, il faut changer le chemin, par exemple
+#export PYTHONPATH=/home/anas/Documents/Python/Robotic_chess:$PYTHONPATH 
+
+import threading  # Import the threading module
+from serial_communication import *
 
 class RoboticArm(IRoboticArmModule):
-    def __init__(self):
+    def __init__(self, selected_port = "COM5"):
         """
         Initialize the RoboticArm class.
         This is a placeholder implementation.
         The actual implementation will involve controlling the robotic arm hardware.
         """
-        self.is_initialized = False
+        self.is_initialized = False 
 
+        self.selected_port = selected_port
+        self.com = SerialCommunication()
+
+        self.loop_thread = None  # Thread for the loop function
+        self.running = False     # Flag to control the loop execution
+        
+        self.current_pos = Position(0,0,0)
+        
+        self.chessboard_moves = ChessboardMoves()
+        self.state_chessboard_manager = 0
+        self.last_move = "" #le dernier move fait par exemple "e2e4"
+
+    def _start_serial(self, selected_port):
+        if selected_port:
+            try:
+                self.com = SerialCommunication(selected_port, SERIAL_BAUDRATE)
+                self.com.start()
+                print("Starting serial self.com ", selected_port)
+            except serial.SerialException as e:
+                print("Serial Error", f"Failed to open port {selected_port}: {e}")
+                
+    def _start_loop(self):
+        """
+        Start the loop function in a separate thread.
+        """
+        if self.loop_thread is None or not self.loop_thread.is_alive():
+            self.running = True
+            print("Loop thread started.")
+            self.loop_thread = threading.Thread(target=self.loop, daemon=True)
+            self.loop_thread.start()
+            
+    def _stop_loop(self):
+        """
+        Stop the loop function.
+        """
+        self.running = False
+        if self.loop_thread is not None:
+            self.loop_thread.join()
+            print("Loop thread stopped.")
+            
     def initialize_robot(self) -> None:
         """
         Initialize the robotic arm module.
         This function sets up the robotic arm hardware (e.g., motors, servos).
         """
+        self._start_serial(self.selected_port) # Start the serial communication
         print("Robotic arm initialized.")
+        self._start_loop()  # Start the loop thread
         self.is_initialized = True
 
     def execute_move(self, move: str) -> None:
@@ -33,11 +80,126 @@ class RoboticArm(IRoboticArmModule):
         if not self.is_initialized:
             raise RuntimeError("Robotic arm is not initialized. Call initialize_robot() first.")
 
+        self.chessboard_moves.addMoves(move)
+        
         print(f"Executing move: {move}")
 
     def shutdown(self) -> None:
         """
         Shut down the robotic arm and clean up resources.
         """
+        self._stop_loop()  # Stop the loop thread
         print("Robotic arm shut down.")
         self.is_initialized = False
+        
+    def _rxManage(self):
+        # print("RxManage")
+        self.com.FIFO_occupation = self.com.FIFO_Ecriture - self.com.FIFO_lecture
+        if(self.com.FIFO_occupation<0):
+            self.com.FIFO_occupation = self.com.FIFO_occupation + SIZE_FIFO
+        if(self.com.FIFO_max_occupation < self.com.FIFO_occupation):
+            self.com.FIFO_max_occupation = self.com.FIFO_occupation
+        if(self.com.FIFO_occupation == 0):
+            return
+        
+        id = self.com.rxMsg[self.com.FIFO_lecture].id
+        print("\nReceived message from id: ", idComEnText.get(id, "ID inconnu"))
+        
+        match id:
+            case id if id == ID_ACK_GENERAL:
+                print("Ack reçu")
+            case id if id == ID_REPEAT_REQUEST:
+                print("Repeat request")
+            case id if id == ID_ACK_CMD_MOVE:
+                print("Move acknowledgment received")
+            case id if id == ID_ACK_SERVO_GRAB:
+                print("Servo grab acknowledgment received")
+            case id if id == ID_ACK_SERVO_RELEASE:
+                print("Servo release acknowledgment received")
+            case id if id == ID_ACK_HOMING:
+                print("Homing acknowledgment received")
+            case id if id == ID_ACK_SEND_CURRENT_POSITION:
+                print("Current position received")
+                # On recoit en mm, donc on convertit en metres
+                self.current_pos.x = float(self.com.rxMsg[self.com.FIFO_lecture].data[0] + (self.com.rxMsg[self.com.FIFO_lecture].data[1] << 8)) / 1000.0
+                self.current_pos.y = float(self.com.rxMsg[self.com.FIFO_lecture].data[2] + (self.com.rxMsg[self.com.FIFO_lecture].data[3] << 8)) / 1000.0
+                self.current_pos.z = float(self.com.rxMsg[self.com.FIFO_lecture].data[4] + (self.com.rxMsg[self.com.FIFO_lecture].data[5] << 8)) / 1000.0
+                print(self.current_pos)
+            case id if id == ID_CMD_BOUTTON_STATE:
+                print("Button state command received")
+            case _:
+                print(f"Received message from an unknown ID")
+        self.com.FIFO_lecture = (self.com.FIFO_lecture + 1) % SIZE_FIFO
+    
+    def chessBoardMoveManager(self):
+        """
+        Manage the moves on the chessboard.
+        This function will handle the moves.
+        """
+        
+        if(self.chessboard_moves.thereIsNewMoveToDo()):
+            moves = self.chessboard_moves.getNextMove()
+            if self.last_move == moves:
+                print("Move already done")
+                return
+            self.last_move = moves
+            
+            start_pos, end_pos = self.chessboard_moves.convertMovesToPositions(moves)
+            print(f"Move from {start_pos} to {end_pos}")
+            
+            self.com.sendMove(start_pos)#On va au dessus de la piece
+            
+            start_pos.z = HAUTEUR_BRAS
+            self.com.sendMove(start_pos)#On descend à la piece
+            
+            self.com.sendGrabPiece(True)#On attrape la piece
+            
+            start_pos.z = 0
+            self.com.sendMove(start_pos)#On remonte
+            
+            self.com.sendMove(end_pos)#On va à la case
+            
+            end_pos.z = HAUTEUR_BRAS
+            self.com.sendMove(end_pos)#On descend à la case
+            
+            self.com.sendGrabPiece(False)#On relache la piece
+            
+            end_pos.z = 0
+            self.com.sendMove(end_pos)#On remonte
+            
+            #Est ce qu'on fait autre chose aprés? Comme aller appuyer sur un bouton? A voir !!!!!!!!!!!!!!!!!!!!
+    
+    def loop(self):
+        """
+        Main loop for the robotic arm module.
+        This function is called in a thread to continuously check for incoming messages and process them.
+        """
+        last_time_pos = 0
+        print("Starting loop... self.running = ", self.running)
+        while self.running:
+            self._rxManage()
+            
+            # self.chessBoardMoveManager()
+            
+            #On actualise la position courante toutes les secondes
+            if time.time() - last_time_pos > 1:
+                last_time_pos = time.time()
+                # Send a request for the current position
+                # self.com.sendEmpty(ID_SERVO_GRAB)
+                
+        print("Exiting loop... self.running = ", self.running)
+
+if __name__ == "__main__":    
+    robotic_arm = RoboticArm(afficherPortDisponible())
+    # robotic_arm.chessboard_moves.addMoves("a1b2")
+    robotic_arm.initialize_robot()
+    # Simulate some operations
+    # robotic_arm.execute_movee from Position(x: 0.0, y: 0.0, z: 0.0) to Position(x: 0.05, y: 0.05, z: 0.0)("e2e4")
+    
+    # Shutdown the robotic arm
+    # robotic_arm.shutdown()
+    while 1:
+        key = input("Press a key: ")
+        if key == 'a':
+            print("Key 'a' pressed")
+            robotic_arm.com.sendEmpty(ID_SEND_CURRENT_POSITION)
